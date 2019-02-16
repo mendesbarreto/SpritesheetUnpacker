fcbf8adb47ecea73ebadf4f8694a03ee721563e4
Douglas Mendes <mendes-barreto@live.com>
2019-02-16 14:23:14 -0500

    Add fist files to project

---
+++ cmake-build-debug/CMakeFiles/SpritesheetUnpacker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpritesheetUnpacker.dir/main.cpp.o"
+  "SpritesheetUnpacker.pdb"
+  "SpritesheetUnpacker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpritesheetUnpacker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
